15b794763e046638207651a7ddd691bba6a5797d
TeamCity <cdbadmin@claremont.se>
2015-02-02 14:32:29 +0100

    First commit

---
+++ TeamCityMVCTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamCityMVCTest.MvcApplication" Language="C#" %>
---
+++ TeamCityMVCTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityMVCTest.Startup))]
+namespace TeamCityMVCTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
